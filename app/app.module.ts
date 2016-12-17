1276115f434d45983f7dd915fb6359b3781e0c7d
mikaelec <mikaelec@gmail.com>
2016-12-17 08:25:28 +0100

    Added empty game component.

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app.component';
 import { HomeComponent } from './components/home.component';
+import { GameComponent} from './components/game.component';
 import { UserService } from './services/user.service';
 import { SocketService } from './services/socket.service';
 
@@ -11,10 +12,11 @@ import { SocketService } from './services/socket.service';
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent }
+      { path: '', component: HomeComponent },
+      { path: 'game', component: GameComponent }
     ])
   ],
-  declarations: [ AppComponent, HomeComponent ],
+  declarations: [ AppComponent, HomeComponent, GameComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ UserService, SocketService ]
 })
---
+++ app/components/game.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  templateUrl: 'views/game'
+})
+export class GameComponent {
+
+}
