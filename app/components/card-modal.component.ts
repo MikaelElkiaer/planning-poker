088e37dc5a5a66bae7286d43e8886cd0469f76de
mikaelec <mikaelec@gmail.com>
2016-12-19 20:35:28 +0100

    Added cards to card modal.

--- app/components/card-modal.component.ts
+++ app/components/card-modal.component.ts
@@ -5,10 +5,63 @@ import { PokerCard } from '../../DTO/pokerCard';
 
 @Component({
   selector: 'card-modal-component',
-  templateUrl: 'views/card-modal'
+  templateUrl: 'views/card-modal',
+  styles: [`.vote {
+              display: inline-block;
+              margin: 10px;
+              padding: 10px;
+              text-align: center;
+              border-radius: 5px;
+            }
+
+            .pickable:hover {
+              background-color: #f5f5f5;
+              cursor:  pointer;
+            }
+
+            div.poker-card {
+              box-shadow: 2px 2px 4px #bbbbbb;
+              margin: auto;
+              padding-top: 50%;
+              width: 70px;
+              height: 100px;
+              font-size: 28px;
+              text-align: center;
+              border: 2px solid black;
+              border-radius: 5px;
+              -webkit-touch-callout: none;
+              -webkit-user-select: none;
+              -khtml-user-select: none;
+              -moz-user-select: none;
+              -ms-user-select: none;
+              user-select: none;
+            }
+
+            .selected div.poker-card {
+              border-color: #357ebd;
+              color: #357ebd;
+              font-weight: bold;
+            }`]
 })
 export class CardModalComponent {
   @Input() currentCard: PokerCard = PokerCard.NotPicked;
+
+  pickableCards: PokerCard[] = [
+    PokerCard.NotPicked,
+    PokerCard.CoffeeBreak,
+    PokerCard.QuestionMark,
+    PokerCard.Zero,
+    PokerCard.Half,
+    PokerCard.One,
+    PokerCard.Two,
+    PokerCard.Three,
+    PokerCard.Five,
+    PokerCard.Eight,
+    PokerCard.Thirteen,
+    PokerCard.Forty,
+    PokerCard.Hundred,
+    PokerCard.Infinity
+  ];
 
   constructor(public activeModal: NgbActiveModal) {}
 }
--- app/pipes/cardText.pipe.ts
+++ app/pipes/cardText.pipe.ts
@@ -7,15 +7,17 @@ export class CardTextPipe implements PipeTransform {
   transform(value: PokerCard): string {
     switch (value) {
       case PokerCard.NotPicked:
-        return '<span class="fa fa-thumbs-o-down vcenter"></span>';
+        return '<span class="fa fa-thumbs-o-down"></span>';
       case PokerCard.Picked:
-        return '<span class="fa fa-thumbs-o-up vcenter"></span>';
+        return '<span class="fa fa-thumbs-o-up"></span>';
       case PokerCard.CoffeeBreak:
-        return '<span class="fa fa-coffee vcenter"></span>';
+        return '<span class="fa fa-coffee"></span>';
       case PokerCard.QuestionMark:
-        return '<span class="fa fa-question vcenter></span>';
+        return '<span class="fa fa-question"></span>';
       case PokerCard.Half:
         return '&frac12;';
+      case PokerCard.Infinity:
+        return '&#x221e;';
       default:
         return value.toString();
     }
