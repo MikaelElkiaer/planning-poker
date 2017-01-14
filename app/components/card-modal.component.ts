439bd7feb8d0877e9e80c5494748b8a642cc7e02
mikaelec <mikaelec@gmail.com>
2017-01-15 00:19:21 +0100

    Changed to dark mode. Updated cards and spinner.

--- app/components/card-modal.component.ts
+++ app/components/card-modal.component.ts
@@ -5,43 +5,7 @@ import * as Dto from '../../shared/dto';
 
 @Component({
   selector: 'card-modal-component',
-  templateUrl: 'views/card-modal',
-  styles: [`.vote {
-              display: inline-block;
-              margin: 10px;
-              padding: 10px;
-              text-align: center;
-              border-radius: 5px;
-            }
-
-            .pickable:hover {
-              background-color: #f5f5f5;
-              cursor:  pointer;
-            }
-
-            div.poker-card {
-              box-shadow: 2px 2px 4px #bbbbbb;
-              margin: auto;
-              padding-top: 50%;
-              width: 70px;
-              height: 100px;
-              font-size: 28px;
-              text-align: center;
-              border: 2px solid black;
-              border-radius: 5px;
-              -webkit-touch-callout: none;
-              -webkit-user-select: none;
-              -khtml-user-select: none;
-              -moz-user-select: none;
-              -ms-user-select: none;
-              user-select: none;
-            }
-
-            .selected div.poker-card {
-              border-color: #357ebd;
-              color: #357ebd;
-              font-weight: bold;
-            }`]
+  templateUrl: 'views/card-modal'
 })
 export class CardModalComponent {
   @Input() currentCard: Dto.PokerCard = Dto.PokerCard.NotPicked;
--- app/components/game.component.ts
+++ app/components/game.component.ts
@@ -8,56 +8,7 @@ import { SocketService, UserService } from '../services/index';
 import { CardModalComponent, KickModalComponent } from './index';
 
 @Component({
-  templateUrl: 'views/game',
-  styles: [`.vote {
-              display: inline-block;
-              margin: 10px;
-              padding: 10px;
-              text-align: center;
-              border-radius: 5px;
-            }
-
-            div.user, div.user div.poker-card {
-              border-color: #357ebd;
-              color: #357ebd;
-            }
-
-            .pickable:hover {
-              background-color: #f5f5f5;
-              cursor:  pointer;
-            }
-
-            div.poker-card {
-              box-shadow: 2px 2px 4px #bbbbbb;
-              margin: auto;
-              padding-top: 50%;
-              width: 70px;
-              height: 100px;
-              font-size: 28px;
-              text-align: center;
-              border: 2px solid black;
-              border-radius: 5px;
-              -webkit-touch-callout: none;
-              -webkit-user-select: none;
-              -khtml-user-select: none;
-              -moz-user-select: none;
-              -ms-user-select: none;
-              user-select: none;
-            }
-
-            .selected div.poker-card {
-              border-color: #357ebd;
-              color: #357ebd;
-              font-weight: bold;
-            }
-            
-            .inactive {
-              color: #ff4136;
-            }
-            
-            .username.inactive {
-              font-style: italic;
-            }`]
+  templateUrl: 'views/game'
 })
 export class GameComponent implements OnDestroy, OnInit {
   get gameId() { return this._gameId; }
