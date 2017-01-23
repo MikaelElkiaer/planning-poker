609e74a66ec1f2339ff3572689d7ac3cb8c37b34
mikaelec <mikaelec@gmail.com>
2017-01-23 20:06:43 +0100

    Added environment config, for enabling prod mode.

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+
 import { AppModule } from './app.module';
+import { environment } from './config/environment';
+
+if (environment.production)
+    enableProdMode();
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
