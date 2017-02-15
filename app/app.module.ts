844e6b2158b49489a1a68824d1ac6ccf87bb292c
mikaelec <mikaelec@gmail.com>
2017-02-15 21:14:37 +0100

    Cleaned up app module and index view.

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,15 +5,10 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToasterModule } from 'angular2-toaster';
 
-import { AppComponent } from './components/app.component';
-import { HomeComponent } from './components/home.component';
-import { GameComponent} from './components/game.component';
-import { CardModalComponent } from './components/card-modal.component';
-import { KickModalComponent } from './components/kick-modal.component';
-import { UserNameModalComponent } from './components/username-modal.component';
+import { AppComponent, HomeComponent, GameComponent } from './components/index';
+import { CardModalComponent, KickModalComponent, UserNameModalComponent } from './components/index';
 import { CardTextPipe } from './pipes/cardText.pipe';
-import { UserService } from './services/user.service';
-import { SocketService } from './services/socket.service';
+import { UserService, SocketService } from './services/index';
 
 @NgModule({
   imports: [
